1e56219cf856e3cbe16cfe8cd0dc4b6a8c9b356c
T45K <tasktas9@gmail.com>
2025-08-23 21:50:12 +0900

    reproducer

--- app/src/test/kotlin/io/github/t45k/kotest/instanceperleaf/alt/Test.kt
+++ app/src/test/kotlin/io/github/t45k/kotest/instanceperleaf/alt/Test.kt
@@ -2,31 +2,35 @@ package io.github.t45k.kotest.instanceperleaf.alt
 
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.matchers.shouldBe
 
 class Test : DescribeSpec({
     isolationMode = IsolationMode.InstancePerLeaf
 
-    var count = 0
-
     describe("d1") {
-        count++
+        println("d1")
+
         context("c1") {
+            println("c1")
+
             it("i1") {
-                count shouldBe 1
+                println("i1")
             }
         }
 
         context("c2") {
+            println("c2")
+
             it("i2") {
-                count shouldBe 1
+                println("i2")
             }
         }
     }
 
     describe("d2") {
+        println("d2")
+
         it("i3") {
-            count shouldBe 0
+            println("i3")
         }
     }
 })
