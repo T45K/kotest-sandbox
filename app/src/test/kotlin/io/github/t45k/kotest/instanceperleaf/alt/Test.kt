1da6d2fafb62093b00357eaafd9a5b560ddc156f
T45K <tasktas9@gmail.com>
2025-09-05 14:08:50 +0900

    beforeSpec reproducer

--- app/src/test/kotlin/io/github/t45k/kotest/instanceperleaf/alt/Test.kt
+++ app/src/test/kotlin/io/github/t45k/kotest/instanceperleaf/alt/Test.kt
@@ -2,61 +2,28 @@ package io.github.t45k.kotest.instanceperleaf.alt
 
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+private var count = 0
 
 class Test : DescribeSpec({
     isolationMode = IsolationMode.InstancePerLeaf
 
+    beforeSpec {
+        count++
+    }
+
     describe("d1") {
-        println("d1")
-
         context("c1") {
-            println("c1")
-
             it("i1") {
-                println("i1")
+                count shouldBe 1
             }
         }
 
         context("c2") {
-            println("c2")
-
             it("i2") {
-                println("i2")
+                count shouldBe 2
             }
         }
     }
 })
-
-/*
-results of Kotest 6.0.1
-d1
-c1
-i1
-c2
-d1
-c2
-i2
- */
-
-/*
-results of Kotest 6.0.0
-d1
-c1
-d1
-c1
-i1
-c2
-d1
-c2
-i2
- */
-
-/*
-results of Kotest 5.9.x
-d1
-c1
-i1
-d1
-c2
-i2
- */
